e4d7dca7fc96b34569d5b267f816013adcfd8b24
Igor Stojadinovic <igorstojadinovic.vs@gmail.com>
2024-05-24 21:20:14 +0200

    Update list of allowed origins
    The list of allowed origins in the backend configuration has been simplified. The previous set of origins mainly consisted of localhost addresses for testing and production addresses for a certain shop, but these have been replaced by a single address for the tech-notes-teeb site on render.

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,13 +1,20 @@
 const allowedOrigins = [
-    'http://localhost:3000',
-    'http://localhost:3500',
-    'http://localhost:5174',
-    'http://localhost:5173',
-    'http://localhost:5173/login',
-    'http://localhost:5173/dashboard/users',
-    'http://localhost:5173/dashboard/notes',
-    'https://www.dandrepairshop.com',
-    'https://dandrepairshop.com',
+    'https://tech-notes-teeb.onrender.com/'
 ];
 
 module.exports = allowedOrigins;
+
+
+/**
+ *
+ *     'http://localhost:3000',
+ *     'http://localhost:3500',
+ *     'http://localhost:5174',
+ *     'http://localhost:5173',
+ *     'http://localhost:5173/login',
+ *     'http://localhost:5173/dashboard/users',
+ *     'http://localhost:5173/dashboard/notes',
+ *     'https://www.dandrepairshop.com',
+ *     'https://dandrepairshop.com',
+ *
+ */
