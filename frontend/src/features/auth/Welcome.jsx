0f591e73aa2bab4e14d813262872bbc392e54c60
Igor Stojadinovic <igorstojadinovic.vs@gmail.com>
2024-06-02 19:24:04 +0200

    Update                                                   Code formate,old classes removed.

--- frontend/src/features/auth/Welcome.jsx
+++ frontend/src/features/auth/Welcome.jsx
@@ -1,31 +1,38 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import useAuth from "../../hooks/useAuth.js";
+
 const Welcome = () => {
-    const { username, isManager, isAdmin} = useAuth()
+  const { username, isManager, isAdmin } = useAuth();
 
-    const date = new Date()
-    const today = new Intl.DateTimeFormat('en-US',{dateStyle:'full',timeStyle:'long'}).format(date)
+  const date = new Date();
+  const today = new Intl.DateTimeFormat("en-US", {
+    dateStyle: "full",
+    timeStyle: "long",
+  }).format(date);
 
-    const content = (
-        <section className="welcome">
-            <p>{today}</p>
-            <h1>Welcome {username}!</h1>
-            <p><Link to='/dashboard/notes'>View techNotes</Link></p>
-            <p><Link to='/dashboard/notes/new'>Add new techNote</Link></p>
-            {
-                (isManager || isAdmin) && <p><Link to='/dashboard/users'>View user settings</Link></p>
+  const content = (
+    <section className="flex flex-col items-center  justify-center  text-slate-600 flex-1">
+      <p>{today}</p>
+      <h1>Welcome {username}!</h1>
+      <p>
+        <Link to="/dashboard/notes">View techNotes</Link>
+      </p>
+      <p>
+        <Link to="/dashboard/notes/new">Add new techNote</Link>
+      </p>
+      {(isManager || isAdmin) && (
+        <p>
+          <Link to="/dashboard/users">View user settings</Link>
+        </p>
+      )}
+      {(isManager || isAdmin) && (
+        <p>
+          <Link to="/dashboard/users/new">Add new user</Link>
+        </p>
+      )}
+    </section>
+  );
 
-            }
-            {
-                (isManager || isAdmin) && <p><Link to='/dashboard/users/new'>Add new user</Link></p>
-            }
-
-
-        </section>
-    )
-
-    return (
-        content
-    )
-}
-export default Welcome
+  return content;
+};
+export default Welcome;
