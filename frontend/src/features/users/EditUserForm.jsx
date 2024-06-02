2c5f2ac4dd01ea771e5589bada7d0875a3a64234
Igor Stojadinovic <igorstojadinovic.vs@gmail.com>
2024-06-02 19:10:46 +0200

    Update                                                   Code formated,removed old css classes

--- frontend/src/features/users/EditUserForm.jsx
+++ frontend/src/features/users/EditUserForm.jsx
@@ -1,184 +1,259 @@
-import { useState, useEffect } from "react"
-import { useUpdateUserMutation, useDeleteUserMutation } from "./usersApiSlice"
-import { useNavigate } from "react-router-dom"
-import { CiSaveUp1,CiTrash} from "react-icons/ci";
+import { useState, useEffect } from "react";
+import { useUpdateUserMutation, useDeleteUserMutation } from "./usersApiSlice";
+import { useNavigate } from "react-router-dom";
+import { CiSaveUp1, CiTrash } from "react-icons/ci";
+import { clsx } from "clsx";
+import { ROLES } from "../../config/roles";
 
-import { ROLES } from "../../config/roles"
-
-const USER_REGEX = /^[A-z]{3,50}$/
-const PWD_REGEX = /^[A-z0-9!@#$%]{4,12}$/
+const USER_REGEX = /^[A-Za-z0-9]{3,50}$/;
+const PWD_REGEX = /^[A-z0-9!@#$%]{4,12}$/;
 
 const EditUserForm = ({ user }) => {
+  const [updateUser, { isLoading, isSuccess, isError, error }] =
+    useUpdateUserMutation();
 
-    const [updateUser, {
-        isLoading,
-        isSuccess,
-        isError,
-        error
-    }] = useUpdateUserMutation()
+  const [
+    deleteUser,
+    { isSuccess: isDelSuccess, isError: isDelError, error: delerror },
+  ] = useDeleteUserMutation();
 
-    const [deleteUser, {
-        isSuccess: isDelSuccess,
-        isError: isDelError,
-        error: delerror
-    }] = useDeleteUserMutation()
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const [username, setUsername] = useState(user.username);
+  const [validUsername, setValidUsername] = useState(false);
+  const [password, setPassword] = useState("");
+  const [validPassword, setValidPassword] = useState(false);
+  const [roles, setRoles] = useState(user.roles);
+  const [active, setActive] = useState(user.active);
 
-    const [username, setUsername] = useState(user.username)
-    const [validUsername, setValidUsername] = useState(false)
-    const [password, setPassword] = useState('')
-    const [validPassword, setValidPassword] = useState(false)
-    const [roles, setRoles] = useState(user.roles)
-    const [active, setActive] = useState(user.active)
+  useEffect(() => {
+    setValidUsername(USER_REGEX.test(username));
+  }, [username]);
 
-    useEffect(() => {
-        setValidUsername(USER_REGEX.test(username))
-    }, [username])
+  useEffect(() => {
+    setValidPassword(PWD_REGEX.test(password));
+  }, [password]);
 
-    useEffect(() => {
-        setValidPassword(PWD_REGEX.test(password))
-    }, [password])
-
-    useEffect(() => {
-
-        if (isSuccess || isDelSuccess) {
-            setUsername('')
-            setPassword('')
-            setRoles([])
-            navigate('/dashboard/users')
-        }
-
-    }, [isSuccess, isDelSuccess, navigate])
-
-    const onUsernameChanged = e => setUsername(e.target.value)
-    const onPasswordChanged = e => setPassword(e.target.value)
-
-    const onRolesChanged = e => {
-        const values = Array.from(
-            e.target.selectedOptions,
-            (option) => option.value
-        )
-        setRoles(values)
+  useEffect(() => {
+    if (isSuccess || isDelSuccess) {
+      setUsername("");
+      setPassword("");
+      setRoles([]);
+      navigate("/dashboard/users");
     }
+  }, [isSuccess, isDelSuccess, navigate]);
 
-    const onActiveChanged = () => setActive(prev => !prev)
+  const onUsernameChanged = (e) => setUsername(e.target.value);
+  const onPasswordChanged = (e) => setPassword(e.target.value);
 
-    const onSaveUserClicked = async (e) => {
-        if (password) {
-            await updateUser({ id: user.id, username, password, roles, active })
-        } else {
-            await updateUser({ id: user.id, username, roles, active })
-        }
-    }
+  const onRolesChanged = (e) => {
+    const values = Array.from(
+      e.target.selectedOptions,
+      (option) => option.value,
+    );
+    setRoles(values);
+  };
 
-    const onDeleteUserClicked = async () => {
-        await deleteUser({ id: user.id })
-    }
+  const onActiveChanged = () => setActive((prev) => !prev);
 
-    const options = Object.values(ROLES).map(role => {
-        return (
-            <option
-                key={role}
-                value={role}
-
-            > {role}</option >
-        )
-    })
-
-    let canSave
+  const onSaveUserClicked = async (e) => {
     if (password) {
-        canSave = [roles.length, validUsername, validPassword].every(Boolean) && !isLoading
+      await updateUser({ id: user.id, username, password, roles, active });
     } else {
-        canSave = [roles.length, validUsername].every(Boolean) && !isLoading
+      await updateUser({ id: user.id, username, roles, active });
     }
+  };
 
-    const errClass = (isError || isDelError) ? "errmsg" : "offscreen"
-    const validUserClass = !validUsername ? 'form__input--incomplete' : ''
-    const validPwdClass = password && !validPassword ? 'form__input--incomplete' : ''
-    const validRolesClass = !(roles.length) ? 'form__input--incomplete' : ''
+  const onDeleteUserClicked = async () => {
+    await deleteUser({ id: user.id });
+  };
 
-    const errContent = (error?.data?.message || delerror?.data?.message) ?? ''
+  const options = Object.values(ROLES).map((role) => {
+    return (
+      <option key={role} value={role} className="capitalize pb-2">
+        {" "}
+        {role}
+      </option>
+    );
+  });
 
+  let canSave;
+  if (password) {
+    canSave =
+      [roles.length, validUsername, validPassword].every(Boolean) && !isLoading;
+  } else {
+    canSave = [roles.length, validUsername].every(Boolean) && !isLoading;
+  }
 
-    const content = (
-        <>
-            <p className={errClass}>{errContent}</p>
+  const errClass = isError || isDelError ? "errmsg" : "offscreen";
 
-            <form className="form" onSubmit={e => e.preventDefault()}>
-                <div className="form__title-row">
-                    <h2>Edit User</h2>
-                    <div className="form__action-buttons">
-                        <button
-                            className="icon-button"
-                            title="Save"
-                            onClick={onSaveUserClicked}
-                            disabled={!canSave}
-                        >
-                            <CiSaveUp1/>
-                        </button>
-                        <button
-                            className="icon-button"
-                            title="Delete"
-                            onClick={onDeleteUserClicked}
-                        >
-                            <CiTrash />
-                        </button>
-                    </div>
-                </div>
-                <label className="form__label" htmlFor="username">
-                    Username: <span className="nowrap">[3-50 letters]</span></label>
-                <input
-                    className={`form__input ${validUserClass}`}
-                    id="username"
-                    name="username"
-                    type="text"
-                    autoComplete="off"
-                    value={username}
-                    onChange={onUsernameChanged}
-                />
+  const errContent = (error?.data?.message || delerror?.data?.message) ?? "";
 
-                <label className="form__label" htmlFor="password">
-                    Password: <span className="nowrap">[empty = no change]</span> <span className="nowrap">[4-12 chars incl. !@#$%]</span></label>
-                <input
-                    className={`form__input ${validPwdClass}`}
-                    id="password"
-                    name="password"
-                    type="password"
-                    value={password}
-                    onChange={onPasswordChanged}
-                />
+  const content = (
+    <>
+      <p
+        className={clsx("", {
+          "text-red-500 transition-all duration-300 visible text-[15px]":
+            isError || isDelError,
+        })}
+      >
+        {errContent}
+      </p>
 
-                <label className="form__label form__checkbox-container" htmlFor="user-active">
-                    ACTIVE:
-                    <input
-                        className="form__checkbox"
-                        id="user-active"
-                        name="user-active"
-                        type="checkbox"
-                        checked={active}
-                        onChange={onActiveChanged}
-                    />
-                </label>
+      <form
+        className="border rounded-lg text-slate-600 flex flex-col  w-1/2 flex-1"
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <div className="bg-gray-200 rounded-t-lg flex justify-between p-5">
+          <h2>Edit User</h2>
+          <div className="flex gap-8">
+            <button
+              title="Save"
+              onClick={onSaveUserClicked}
+              disabled={!canSave}
+            >
+              <CiSaveUp1 size={28} />
+            </button>
+            <button title="Delete" onClick={onDeleteUserClicked}>
+              <CiTrash className="text-red-500" size={28} />
+            </button>
+          </div>
+        </div>
 
-                <label className="form__label" htmlFor="roles">
-                    ASSIGNED ROLES:</label>
-                <select
-                    id="roles"
-                    name="roles"
-                    className={`form__select ${validRolesClass}`}
-                    multiple={true}
-                    size="3"
-                    value={roles}
-                    onChange={onRolesChanged}
-                >
-                    {options}
-                </select>
+        <div className="p-5 flex flex-col gap-8">
+          <div className="flex flex-col gap-4 justify-center">
+            <div className="flex justify-between items-center">
+              <label htmlFor="username">Username:</label>
 
-            </form>
-        </>
-    )
+              <input
+                className={clsx(
+                  "",
+                  {
+                    "border border-red-500 rounded w-1/2 py-2 px-3 text-slate-500 mb-3 leading-tight focus:outline-none focus:shadow-outline":
+                      !validUsername,
+                  },
+                  {
+                    "border border-slate-200 rounded w-1/2 py-2 px-3 text-slate-500 mb-3 leading-tight focus:outline-none focus:shadow-outline":
+                      validUsername,
+                  },
+                )}
+                id="username"
+                name="username"
+                type="text"
+                autoComplete="off"
+                value={username}
+                onChange={onUsernameChanged}
+              />
+            </div>
+            <p
+              className={clsx(
+                "text-red-500 transition-all duration-300 visible text-[15px] ",
+                {
+                  "opacity-0": validUsername,
+                },
+              )}
+            >
+              Invalid username,must be [3-50 letters], can't contain any special
+              characters -@*+$%& etc..
+            </p>
+          </div>
 
-    return content
-}
-export default EditUserForm
+          <div className="flex flex-col gap-4">
+            <div className="flex justify-between items-center">
+              <label htmlFor="password">
+                Password: <span className="nowrap">[empty = no change]</span>{" "}
+              </label>
+              <input
+                className={clsx(
+                  "",
+                  {
+                    "border border-red-500 rounded w-1/2 py-2 px-3 text-slate-500 mb-3 leading-tight focus:outline-none focus:shadow-outline":
+                      validPassword,
+                  },
+                  {
+                    "border border-slate-200 rounded w-1/2 py-2 px-3 text-slate-500 mb-3 leading-tight focus:outline-none focus:shadow-outline":
+                      !validPassword,
+                  },
+                )}
+                id="password"
+                name="password"
+                type="password"
+                value={password}
+                onChange={onPasswordChanged}
+              />
+            </div>
+
+            <p
+              className={clsx(
+                "text-red-500 transition-all duration-300 visible text-[15px]",
+                {
+                  "opacity-0": !validPassword,
+                },
+              )}
+            >
+              Invalid password,must contain [4-12 chars incl. !@#$%]
+            </p>
+          </div>
+
+          <div className="flex gap-8">
+            <label
+              className="form__label form__checkbox-container"
+              htmlFor="user-active"
+            >
+              ACTIVE:
+            </label>
+
+            <input
+              className="form__checkbox"
+              id="user-active"
+              name="user-active"
+              type="checkbox"
+              checked={active}
+              onChange={onActiveChanged}
+            />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <p
+              className={clsx("opacity-0", {
+                "text-red-500 transition-all duration-300 visible text-[15px]":
+                  !canSave,
+              })}
+            >
+              Please select the role.
+            </p>
+            <div className="flex justify-between">
+              <label className="form__label" htmlFor="roles">
+                ASSIGNED ROLES
+              </label>
+              <select
+                id="roles"
+                name="roles"
+                className={clsx(
+                  "",
+                  {
+                    "p-10 border focus:outline-none focus:shadow-outline overflow-hidden":
+                      roles,
+                  },
+                  {
+                    "p-5 border-none focus:outline-none focus:shadow-outline overflow-hidden":
+                      !roles,
+                  },
+                )}
+                multiple={true}
+                size="3"
+                value={roles}
+                onChange={onRolesChanged}
+              >
+                {options}
+              </select>
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  );
+
+  return content;
+};
+export default EditUserForm;
