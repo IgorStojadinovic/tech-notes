6d9cabf10d8aa58629c88b7d47562baa9befcd79
Igor Stojadinovic <igorstojadinovic.vs@gmail.com>
2024-06-02 21:02:21 +0200

    Update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import { store } from './app/store'
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+import { FaGithub } from "react-icons/fa";
 
-if (process.env.NODE_ENV === 'production') disableReactDevTools()
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <Provider store={store} >
-          <BrowserRouter>
-              <Routes>
-                  <Route path="/*" element={<App />} />
-              </Routes>
-          </BrowserRouter>
-      </Provider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <a
+          href="https://github.com/IgorStojadinovic/tech-notes"
+          target="_blank"
+          className="absolute bottom-0 right-0"
+        >
+          <FaGithub size={40} />
+        </a>
 
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
-)
+);
