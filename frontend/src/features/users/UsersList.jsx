517f2985f89dd1104c744e4c7688470ed3006b79
Igor Stojadinovic <igorstojadinovic.vs@gmail.com>
2024-06-02 19:23:39 +0200

    Update                                                   Table update,removed old classes.

--- frontend/src/features/users/UsersList.jsx
+++ frontend/src/features/users/UsersList.jsx
@@ -1,51 +1,48 @@
 import { useGetUsersQuery } from "./usersApiSlice.js";
-import User from "./User.jsx"
+import User from "./User.jsx";
 import PulseLoader from "react-spinners/PulseLoader";
+import useAuth from "../../hooks/useAuth.js";
 
 const UsersList = () => {
-    const {
-        data : users , isLoading , isSuccess , isError , error
-    } = useGetUsersQuery ('usersList',{
-        pollInterval:60000,refetchOnFocus: true,refetchOnMountOrArgChange: true})
-    let content
+  const {
+    data: users,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetUsersQuery("usersList", {
+    pollInterval: 60000,
+    refetchOnFocus: true,
+    refetchOnMountOrArgChange: true,
+  });
+  let content;
+  if (isLoading) content = <PulseLoader color={"#FFF"} />;
 
-    if (isLoading) content = <PulseLoader color={"#FFF"}/>
+  if (isError) {
+    content = <p className="errmsg">{error.error}</p>;
+  }
 
-    if (isError) {
+  if (isSuccess) {
+    const { ids } = users;
 
-        content = <p className='errmsg'>{ error.error }</p>
-    }
+    const tableContent = ids?.length
+      ? ids.map((userId) => <User key={userId} userId={userId} />)
+      : null;
 
-    if (isSuccess) {
+    content = (
+      <table className="grid grid-cols-1 border font-medium  text-slate-400   text-left rounded-lg ">
+        <thead>
+          <tr className="flex justify-between w-full p-5 bg-gray-200 rounded-lg text-center ">
+            <th className="flex-1">Username</th>
+            <th className="flex-1">Roles</th>
+            <th className="flex-1">Edit</th>
+          </tr>
+        </thead>
+        <tbody>{tableContent}</tbody>
+      </table>
+    );
+  }
 
-        const { ids } = users
-
-        const tableContent = ids?.length ? ids.map ( userId => <User key={ userId } userId={ userId }/> ) : null
-
-
-        content = (<table className="table table--users">
-            <thead className="table__thead">
-                <tr>
-                    <th scope='col' className='table__th user__username'>
-                        Username
-                    </th>
-                    <th scope='col' className='table__th user__roles'>
-                        Roles
-                    </th>
-                    <th scope='col' className='table__th user__edit'>
-                        Edit
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-            { tableContent }
-            </tbody>
-
-        </table>)
-
-    }
-
-
-    return (content)
-}
-export default UsersList
+  return content;
+};
+export default UsersList;
