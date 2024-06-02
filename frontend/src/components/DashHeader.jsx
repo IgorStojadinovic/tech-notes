38dda8cb52d7c324a5dda87faed4f88f6f9e1009
Igor Stojadinovic <igorstojadinovic.vs@gmail.com>
2024-06-02 18:48:24 +0200

    Update                                                   Code formated, removed old css classes...

--- frontend/src/components/DashHeader.jsx
+++ frontend/src/components/DashHeader.jsx
@@ -1,147 +1,125 @@
-import { Link,useNavigate,useLocation } from "react-router-dom";
-import { IoIosLogOut,IoIosSettings  } from "react-icons/io";
-import { MdEdit } from "react-icons/md";
+import { useLocation, useNavigate } from "react-router-dom";
+import { IoIosLogOut, IoIosSettings } from "react-icons/io";
+import { IoSettingsOutline } from "react-icons/io5";
+import { CiEdit } from "react-icons/ci";
+
 import { CiCirclePlus } from "react-icons/ci";
 import { useSendLogoutMutation } from "../features/auth/authApiSlice.js";
-import {useEffect} from "react";
+import { useEffect } from "react";
 import useAuth from "../hooks/useAuth.js";
 
-
-const DASH_REGEX = /^\/dashboard(\/)?$/
-const NOTES_REGEX = /^\/dashboard\/notes(\/)?$/
-const USERS_REGEX = /^\/dashboard\/users(\/)?$/
-
+const DASH_REGEX = /^\/dashboard(\/)?$/;
+const NOTES_REGEX = /^\/dashboard\/notes(\/)?$/;
+const USERS_REGEX = /^\/dashboard\/users(\/)?$/;
 
 const DashHeader = () => {
-    const { isManager, isAdmin}  = useAuth();
-    const navigate = useNavigate()
-    const { pathname } = useLocation()
+  const { isManager, isAdmin } = useAuth();
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
 
-    const [sendLogout, {
-        isLoading,
-        isSuccess,
-        isError,
-        error
-    }] = useSendLogoutMutation()
+  const [sendLogout, { isLoading, isSuccess, isError, error }] =
+    useSendLogoutMutation();
 
-    useEffect(() => {
-        if (isSuccess) navigate('/')
-    }, [isSuccess,navigate]);
+  useEffect(() => {
+    if (isSuccess) navigate("/");
+  }, [isSuccess, navigate]);
 
-    const onNewNoteClicked = () => navigate('/dashboard/notes/new')
-    const onNewUserClicked = () => navigate('/dashboard/users/new')
-    const onNotesClicked= () => navigate('/dashboard/notes')
-    const onUsersClicked = () => navigate('/dashboard/users')
+  const onNewNoteClicked = () => navigate("/dashboard/notes/new");
+  const onNewUserClicked = () => navigate("/dashboard/users/new");
+  const onNotesClicked = () => navigate("/dashboard/notes");
+  const onUsersClicked = () => navigate("/dashboard/users");
 
-    const onLogoutClicked = () => sendLogout();
+  const onLogoutClicked = () => sendLogout();
 
+  let newNoteButton = null;
+  if (NOTES_REGEX.test(pathname)) {
+    newNoteButton = (
+      <button title="New Note" onClick={onNewNoteClicked}>
+        <CiCirclePlus
+          className="text-slate-800 transition-all duration-300 hover:text-slate-500"
+          size={24}
+        />
+      </button>
+    );
+  }
 
-    let dashClass = null
+  let newUserButton = null;
+  if (USERS_REGEX.test(pathname)) {
+    newUserButton = (
+      <button title="New User" onClick={onNewUserClicked}>
+        <CiCirclePlus
+          className="text-slate-800 transition-all duration-300 hover:text-slate-500"
+          size={24}
+        />
+      </button>
+    );
+  }
 
-    if (!DASH_REGEX.test(pathname) && !NOTES_REGEX.test(pathname) && !USERS_REGEX.test(pathname)) {
-        dashClass = "dash-header__container--small"
-    }
-
-    let newNoteButton = null
-    if (NOTES_REGEX.test(pathname)) {
-        newNoteButton = (
-            <button
-                className="icon-button"
-                title="New Note"
-                onClick={onNewNoteClicked}
-            >
-               <CiCirclePlus/>
-            </button>
-        )
-    }
-
-    let newUserButton = null
-    if (USERS_REGEX.test(pathname)) {
-        newUserButton = (
-            <button
-                className="icon-button"
-                title="New User"
-                onClick={onNewUserClicked}
-            >
-                <CiCirclePlus />
-            </button>
-        )
-    }
-
-    let userButton = null
-    if (isManager || isAdmin) {
-        if (!USERS_REGEX.test(pathname) && pathname.includes('/dashboard')) {
-            userButton = (
-                <button
-                    className="icon-button"
-                    title="Users"
-                    onClick={onUsersClicked}
-                >
-                    <IoIosSettings/>
-                </button>
-            )
-        }
-    }
-
-    let notesButton = null
-    if (!NOTES_REGEX.test(pathname) && pathname.includes('/dashboard')) {
-        notesButton = (
-            <button
-                className="icon-button"
-                title="Notes"
-                onClick={onNotesClicked}
-            >
-                <MdEdit />
-            </button>
-        )
-    }
-
-
-
-
-    const logoutButton = (
-        <button
-            className='icon-button'
-            title='Logout'
-            onClick={onLogoutClicked}
-        >
-            <IoIosLogOut/>
+  let userButton = null;
+  if (isManager || isAdmin) {
+    if (!USERS_REGEX.test(pathname) && pathname.includes("/dashboard")) {
+      userButton = (
+        <button title="Users" onClick={onUsersClicked}>
+          <IoSettingsOutline
+            className="text-slate-800 transition-all duration-300 hover:text-slate-500"
+            size={24}
+          />
         </button>
-    )
-
-    const errClass = isError ? 'errmsg' :' offscreen'
-
-    let buttonContent
-    if (isLoading) {
-        buttonContent = <p>Logging out...</p>
-    } else {
-        buttonContent = (
-            <>
-                {newNoteButton}
-                {newUserButton}
-                {notesButton}
-                {userButton}
-                {logoutButton}
-            </>
-        )
+      );
     }
+  }
 
-    const content = (
-        <>
-            <p className={errClass}>{error?.data?.message}</p>
-        <header className="dash-header">
-            <div className={`dash-header__container ${dashClass}`}>
-                <Link to="/dashboard">
-                    <h1 className="dash-header__title">techNotes</h1>
-                </Link>
-                <nav className="dash-header__nav">
-                    {/* add nav buttons later */}
-                    {buttonContent}
-                </nav>
-            </div>
-        </header>
-        </>
-    )
-    return content
-}
-export default DashHeader
+  let notesButton = null;
+  if (!NOTES_REGEX.test(pathname) && pathname.includes("/dashboard")) {
+    notesButton = (
+      <button title="Notes" onClick={onNotesClicked}>
+        <CiEdit
+          className="text-slate-800 transition-all duration-300 hover:text-slate-500"
+          size={24}
+        />
+      </button>
+    );
+  }
+
+  const logoutButton = (
+    <button title="Logout" onClick={onLogoutClicked}>
+      <IoIosLogOut
+        className="text-slate-800 transition-all duration-300 hover:text-slate-500"
+        size={24}
+      />
+    </button>
+  );
+
+  const errClass = isError ? "errmsg" : " offscreen";
+
+  let buttonContent;
+  if (isLoading) {
+    buttonContent = <p>Logging out...</p>;
+  } else {
+    buttonContent = (
+      <>
+        {newNoteButton}
+        {newUserButton}
+        {notesButton}
+        {userButton}
+        {logoutButton}
+      </>
+    );
+  }
+
+  const content = (
+    <>
+      <header className="bg-gray-200 border-b-2 text-semi-dark-blue p-5">
+        <div className=" ">
+          <p className={errClass}>{error?.data?.message}</p>
+          <nav className="flex gap-6 justify-end items-end ">
+            {/* add nav buttons later */}
+            {buttonContent}
+          </nav>
+        </div>
+      </header>
+    </>
+  );
+  return content;
+};
+export default DashHeader;
