47fbca4b5f7fc16cbb606283ac84384612e799a4
Igor Stojadinovic <igorstojadinovic.vs@gmail.com>
2024-06-02 19:12:30 +0200

    Update                                                   Implemented Toast notifications

--- frontend/src/features/auth/Login.jsx
+++ frontend/src/features/auth/Login.jsx
@@ -1,109 +1,164 @@
-import { useRef, useState ,useEffect} from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setCredentials } from "./authSlice.js";
 import { useLoginMutation } from "./authApiSlice.js";
-import  usePersist  from "../../hooks/usePersist.js"
+import usePersist from "../../hooks/usePersist.js";
 import PulseLoader from "react-spinners/PulseLoader";
 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const Login = () => {
-    const userRef = useRef()
-    const errRef = useRef()
-    const [username,setUsername] = useState('')
-    const [password,setPassword] = useState('')
-    const [errMsg,setErrMsg] = useState('')
-    const [persist,setPersist] = usePersist()
-    const navigate  = useNavigate()
-    const dispatch = useDispatch()
+  const userRef = useRef();
+  const errRef = useRef();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [persist, setPersist] = usePersist();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const [login, {isLoading }] = useLoginMutation()
+  const [login, { isLoading }] = useLoginMutation();
 
-    useEffect(() => {
-        userRef.current.focus()
-    },[])
-
-    useEffect(() => {
-        setErrMsg('')
-    }, [username,password]);
-
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        try {
-            const { accessToken } = await login({username,password}).unwrap()
-            dispatch(setCredentials({accessToken}))
-            setUsername('')
-            setPassword('')
-            navigate('/dashboard')
-        } catch (err) {
-            if (!err.status) {
-                setErrMsg('No Server Response')
-            } else if (err.status === 400) {
-                setErrMsg('Missing username or password')
-            } else if (err.status === 401) {
-                setErrMsg('Unauthorized')
-            } else {
-                setErrMsg(err.data?.message)
-            }
-            errRef.current.focus()
-        }
+  const notify = () => {
+    if (!username) {
+      toast.error(" Invalid username!", {
+        position: "bottom-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
     }
 
-    const handleUserInput = (e) => setUsername(e.target.value)
-    const handlePwdInput = (e) => setPassword(e.target.value)
-    const handleToggle = () => setPersist(prev => !prev)
+    if (!password) {
+      toast.error(" Invalid password!", {
+        position: "bottom-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+  };
 
-    const errClass = errMsg ? 'errmsg' : 'offscreen'
+  const errorNotify = () => {
+    toast.error(" !", {
+      position: "bottom-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  };
 
-    if (isLoading) return <PulseLoader color={"#FFF"}/>
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
 
-    const content = (
-        <section className='public'>
-            <header >
-                <h1>Employee Login</h1>
-            </header>
-            <main className='login'>
-                <p ref={errRef} className={errClass} aria-live='assertive'>{errMsg}</p>
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor='username'>Username:</label>
-                    <input
-                        className='form__input'
-                        type='text'
-                        id='username'
-                        ref={userRef}
-                        value={username}
-                        onChange={handleUserInput}
-                        autoComplete='off'
-                        required
-                    />
-                    <label htmlFor='password'>Password:</label>
-                    <input
-                        className='form__input'
-                        type='password'
-                        id='password'
-                        onChange={handlePwdInput}
-                        value={password}
-                        required
-                    />
-                    <button className='form__submit-button'>Sign in</button>
+  useEffect(() => {
+    setErrMsg("");
+  }, [username, password]);
 
-                    <label htmlFor='persist' className='form__persist'>
-                        <input
-                            type='checkbox'
-                            //className='form__checkbox'
-                            id='perist'
-                            onChange={handleToggle}
-                            checked={persist}
-                        />
-                        Trust This Device
-                    </label>
-                </form>
-            </main>
-            <footer>
-                <Link to="/" >Back to Home</Link>
-            </footer>
-        </section>
-    )
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { accessToken } = await login({ username, password }).unwrap();
+      dispatch(setCredentials({ accessToken }));
+      setUsername("");
+      setPassword("");
+      navigate("/dashboard");
+    } catch (err) {
+      if (!err.status) {
+        setErrMsg("No Server Response");
+      } else if (err.status === 400) {
+        setErrMsg("Missing username or password");
+      } else if (err.status === 401) {
+        setErrMsg("Unauthorized");
+      } else {
+        setErrMsg(err.data?.message);
+      }
+      errRef.current.focus();
+    }
+  };
 
-    return content
-}
-export default Login
+  const handleUserInput = (e) => setUsername(e.target.value);
+  const handlePwdInput = (e) => setPassword(e.target.value);
+  const handleToggle = () => setPersist((prev) => !prev);
+
+  const content = (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div>
+        <ToastContainer />
+      </div>
+      {errMsg && <p>{errMsg}</p>}
+      <section className="flex flex-col bg-semi-dark-blue rounded-lg ">
+        <main className="p-10">
+          <h1 className="mb-8 font-semibold text-2xl"> Login</h1>
+
+          <form onSubmit={handleSubmit} className="flex flex-col gap-10">
+            <input
+              className="bg-transparent border-b-[1px] border-b-greyish-blue px-4 pb-4 outline-0 font-light focus:border-b-red-main"
+              type="text"
+              id="username"
+              ref={userRef}
+              value={username}
+              onChange={handleUserInput}
+              autoComplete="off"
+              required
+              placeholder="Username"
+            />
+
+            <input
+              className="bg-transparent border-b-[1px] border-b-greyish-blue px-4 pb-4 outline-0 font-light focus:border-b-red-main"
+              type="password"
+              id="password"
+              onChange={handlePwdInput}
+              value={password}
+              required
+              placeholder="Password"
+            />
+            <button
+              className=" bg-red-main py-3 rounded-lg font-light hover:bg-red-main/80 transition-color duration-300  hover:bg-light-peach  z-20 drop-shadow-lg"
+              onClick={notify}
+            >
+              Login
+            </button>
+
+            <label htmlFor="persist" className="text-sm flex gap-1.5">
+              <input
+                type="checkbox"
+                id="perist"
+                onChange={handleToggle}
+                checked={persist}
+              />
+              Trust This Device
+            </label>
+          </form>
+          <span className="mt-6 text-center font-ligh text-base">
+            Don't have an account?
+            <Link
+              to={"/signup"}
+              className="ml-2 text-red-main hover:text-greyish-blue transition-color duration-300 "
+            >
+              Sign Up
+            </Link>
+          </span>
+        </main>
+      </section>
+    </div>
+  );
+
+  return content;
+};
+export default Login;
