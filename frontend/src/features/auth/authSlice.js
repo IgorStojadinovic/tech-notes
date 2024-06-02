3298cc31fd2fdcff0722a5c330d94fc9acbab6f6
Igor Stojadinovic <igorstojadinovic.vs@gmail.com>
2024-06-02 18:45:00 +0200

    Update                                                   authSlice.js reducer is updated

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,22 +1,25 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
+
 const authSlice = createSlice({
-    name:'auth',
-    initialState: {token:null},
-    reducers: {
-        setCredentials: (state,action) => {
-            const {accessToken} = action.payload
-            state.token = accessToken
-        },
-        logOut: (state,action) => {
-            state.token = null
-        }
+  name: "auth",
+  initialState: { token: null },
+  reducers: {
+    setCredentials: (state, action) => {
+      const { accessToken } = action.payload;
+      state.token = accessToken;
+    },
+    logOut: (state, action) => {
+      state.token = null;
+    },
+    signUp: (state, action) => {
+      const { accessToken } = action.payload;
+      state.token = accessToken;
+    },
+  },
+});
 
-    }
-})
+export const { setCredentials, logOut, signUp } = authSlice.actions;
 
+export default authSlice.reducer;
 
-export const { setCredentials, logOut } = authSlice.actions
-
-export  default  authSlice.reducer
-
-export const selectCurrentToken = (state) => state.auth.token
+export const selectCurrentToken = (state) => state.auth.token;
