b312dc9f1dc83a0ee8870adf17aad3bc852417a3
Igor Stojadinovic <igorstojadinovic.vs@gmail.com>
2024-06-02 18:46:07 +0200

    Update                                                   Formated code

--- frontend/src/components/DashFooter.jsx
+++ frontend/src/components/DashFooter.jsx
@@ -1,35 +1,36 @@
-
-import {useNavigate,useLocation} from 'react-router-dom';
+import { useNavigate, useLocation } from "react-router-dom";
 import { FaHouseChimney } from "react-icons/fa6";
 import useAuth from "../hooks/useAuth.js";
 
 const DashFooter = () => {
-    const { username , status} = useAuth()
-    const navigate = useNavigate()
-    const { pathname } = useLocation()
+  const { username, status } = useAuth();
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
 
-    const onGoHomeClicked = () => navigate('/dashboard')
+  const onGoHomeClicked = () => navigate("/dashboard");
 
-    let goHomeButton = null
-    if (pathname !== '/dashboard') {
-        goHomeButton = (
-            <button
-                className="dash-footer__button icon-button"
-                title="Home"
-                onClick={onGoHomeClicked}
-            >
-                <FaHouseChimney />
-            </button>
-        )
-    }
+  let goHomeButton = null;
+  if (pathname !== "/dashboard") {
+    goHomeButton = (
+      <button
+        className="dash-footer__button icon-button"
+        title="Home"
+        onClick={onGoHomeClicked}
+      >
+        <FaHouseChimney />
+      </button>
+    );
+  }
 
-    const content = (
-        <footer className="dash-footer">
-            {goHomeButton}
-            <p>Current User: {username}</p>
-            <p>Status: {status}</p>
-        </footer>
-    )
-    return content
-}
-export default DashFooter
+  const content = (
+    <footer className="bg-gray-200 flex border-t-2 p-5 justify-between border text-slate-500 font-medium">
+      {goHomeButton}
+      <div className="flex gap-6">
+        <p>Current User: {username}</p>
+        <p>Status: {status}</p>
+      </div>
+    </footer>
+  );
+  return content;
+};
+export default DashFooter;
