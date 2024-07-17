0050485ee78d50dda2bb652ba1b67c094c9c3b50
Igor Stojadinovic <igorstojadinovic.vs@gmail.com>
2024-07-17 20:53:13 +0200

    animation update

--- frontend/src/features/auth/Welcome.jsx
+++ frontend/src/features/auth/Welcome.jsx
@@ -12,8 +12,13 @@ const Welcome = () => {
 
     const content = (
         <motion.section 
-        animate={{ x: 100 }}
-        transition={{ ease: "easeOut", duration: 2 }}
+        initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01]
+      }}
         className="h-full  bg-main-bg flex flex-col items-center  justify-center  text-slate-600 flex-1 gap-4">
             <div className="text-center bg-white p-20 rounded-lg shadow-lg">
                 <p>{today}</p>
