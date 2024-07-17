aeb7a7048e81b7d2ade1c9213295e7bcd6e57690
Igor Stojadinovic <igorstojadinovic.vs@gmail.com>
2024-07-17 21:05:12 +0200

    animation update

--- frontend/src/features/auth/Welcome.jsx
+++ frontend/src/features/auth/Welcome.jsx
@@ -14,6 +14,7 @@ const Welcome = () => {
         <motion.section 
         initial={{ opacity: 0, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1 }}
+        viewport={{ once: true }}
         transition={{
             delay: 0.5,
             duration: 0.4,
