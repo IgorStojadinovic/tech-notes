b18f56e536a69a6e39d7e5f9f758c45bd10ab5cb
Igor Stojadinovic <igorstojadinovic.vs@gmail.com>
2024-07-17 21:02:24 +0200

    update

--- frontend/src/features/auth/Welcome.jsx
+++ frontend/src/features/auth/Welcome.jsx
@@ -15,8 +15,8 @@ const Welcome = () => {
         initial={{ opacity: 0, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{
-            duration: 0.8,
             delay: 0.5,
+            duration: 0.4,
             ease: [0, 0.71, 0.2, 1.01]
         }}
         className="h-full  bg-main-bg flex flex-col items-center  justify-center  text-slate-600 flex-1 gap-4">
