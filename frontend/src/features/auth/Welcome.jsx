ba3dff5870d3962279fcf357d1d677e9baf520ee
Igor Stojadinovic <igorstojadinovic.vs@gmail.com>
2024-07-17 20:50:17 +0200

    added framer-motion

--- frontend/src/features/auth/Welcome.jsx
+++ frontend/src/features/auth/Welcome.jsx
@@ -1,6 +1,6 @@
 import {Link} from "react-router-dom";
 import useAuth from "../../hooks/useAuth.js";
-
+import motion from "framer-motion"
 const Welcome = () => {
     const {username, isManager, isAdmin} = useAuth();
 
@@ -11,14 +11,17 @@ const Welcome = () => {
     }).format(date);
 
     const content = (
-        <section className="h-full  bg-main-bg flex flex-col items-center  justify-center  text-slate-600 flex-1 gap-4">
+        <motion.section 
+        animate={{ x: 100 }}
+        transition={{ ease: "easeOut", duration: 2 }}
+        className="h-full  bg-main-bg flex flex-col items-center  justify-center  text-slate-600 flex-1 gap-4">
             <div className="text-center bg-white p-20 rounded-lg shadow-lg">
                 <p>{today}</p>
                 <h1>Welcome {username}!</h1>
             </div>
 
 
-        </section>
+        </motion.section>
     );
 
     return content;
